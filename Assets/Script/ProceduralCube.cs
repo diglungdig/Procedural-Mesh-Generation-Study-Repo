eae85f12082b0a9098a21858b5fe243db560db6e
diglungdig <tachyon.of.tear@gmail.com>
2017-07-04 13:59:12 -0400

    Procedural Cube and Grid made of quads

---
+++ Assets/Script/ProceduralCube.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+
+public class ProceduralCube : MonoBehaviour {
+    Mesh mesh;
+    List<Vector3> vertices;
+    List<int> triangles;
+    private void Awake()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        MakeCube();
+        UpdateMesh();
+    }
+
+
+    void MakeCube()
+    {
+        vertices = new List<Vector3>();
+        triangles = new List<int>();
+        for (int i = 0; i < 6; i++)
+        {
+            MakeFace(i);
+        }
+    }
+    void MakeFace(int dir)
+    {
+        vertices.AddRange(CubeMeshData.faceVertices(dir));
+
+        int vCount = vertices.Count;
+        triangles.Add(vCount - 4);
+        triangles.Add(vCount - 4 + 1);
+        triangles.Add(vCount - 4 + 2);
+        triangles.Add(vCount - 4);
+        triangles.Add(vCount - 4 + 2);
+        triangles.Add(vCount - 4 + 3);
+
+    }
+
+
+
+    void UpdateMesh()
+    {
+        mesh.Clear();
+        mesh.SetVertices(vertices);
+        mesh.triangles = triangles.ToArray();
+        mesh.RecalculateNormals();
+    }
+
+}
